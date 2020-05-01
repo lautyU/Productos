e645d3d7ff3fe4eaf558475cbe3f5f97ac4fe61a
lautyU <47756293+lautyU@users.noreply.github.com>
2020-05-01 14:41:53 -0300

    Agregar archivos de proyecto.

---
+++ Dominio/Domain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Precio { get; set; }
+        public int Stock { get; set; }
+
+        public Producto(int id, string nom, string desc, int pre, int st)
+        {
+            IdProducto = id;
+            Nombre = nom;
+            Descripcion = desc;
+            Precio = pre;
+            Stock = st;
+        }
+        public Producto() { }
+    }
+}
---
+++ Paginaweb/Javascript/Script.js
@@ -0,0 +1,18 @@
+document.getElementsByClassName('btn_submit')[0].addEventListener("click", aceptar);
+
+function aceptar(){
+
+  alert("cambios guardados");
+}
+document.getElementsByClassName('btn_reset')[0].addEventListener("click",eliminar);
+
+function eliminar(){
+
+alert("se elimino el producto");
+}
+document.getElementsByID('Nombre').addEventListener("click",comprobar);
+{
+  function comprobar()
+  var Nombre=document.getElementsByID("nombre");
+  nombre.value
+}
